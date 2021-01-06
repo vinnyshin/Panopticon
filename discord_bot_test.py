1e0224a3bb99b6dfb38881ea4fb455fb627e84e3
Hojung Shin <tankmom7279@gmail.com>
2021-01-06 18:43:24 +0900

    !ssh updated

--- discord_bot_test.py
+++ discord_bot_test.py
@@ -5,7 +5,7 @@ from discord.ext import commands
 from stopwatch import Stopwatch
 from datetime import date
 
-token = "토큰은따로"
+token = "토큰"
 app = commands.Bot(command_prefix='!')
 stopwatch_dic = {}
 app.remove_command('help')
@@ -31,9 +31,6 @@ async def on_ready():
     print("name = " + str(app.user.name)) # 봇 이름
     print("id = " + str(app.user.id)) # 봇 고유 ID
     print('Connection was succesful')
-     
-
-#todo help 명령어 완성시키기, 돌릴서버 만들기, 날짜 지나면 초기화
 
 @app.command()
 async def help(ctx) :
@@ -45,13 +42,19 @@ async def help(ctx) :
      !휴식 - 일시정지 # 따로 정지기능은 구현하지 않았습니다.
      !빡공시간 - 지금까지 공부한 시간 출력
      !시간대결 - 누가 더 열심히 공부했나 대결
-     
+     !ssh - 서버 연결 방법
+
      날짜 변경시 자동으로 시간은 초기화 됩니다.
 
      그럼 모두 즐공!
+
+     깃 저장소는 여기로 -> : https://github.com/vinnyshin/Panopticon.git
+     누구나 마음껏 수정하세요!
+
+     gcp기반 서버 오픈했습니다.
+     ssh 퍼블릭키 만들어서 주시면 접속할 수 있게 권한 부여하겠습니다.
      ''', color=0x62c1cc)
     
-    embed.set_footer(text="하단 설명")
     await ctx.send(embed = embed)
 
 @app.command()
@@ -96,5 +99,34 @@ async def 시간대결(ctx):
     for key in stopwatch_dic :
         temp_string += getStudyTime(key) + "\n"
     await ctx.send(temp_string)
+
+@app.command()
+async def ssh(ctx):
+    strTemp = '''
+    __ssh 연결 방법__
+
+    1. 키 값 생성
+    윈도우면 cmd, 리눅스, 맥이면 터미널에
+    
+    ssh-keygen -t rsa -f ~/.ssh/[KEY_FILE_NAME] -C "[USERNAME]"
+    Ex) ssh-keygen -t rsa -f ~/.ssh/gcp_key -C "noname@gmail.com"
+    
+    2. 생성된 [KEY_FILE_NAME].pub 파일을 텍스트 에디터 등을 활용해 내용물을 기록합니다.
+
+    3. 내용물을 관리자에게 전송!
+
+    4. 설정이 완료 되었다면
+
+    ***주의***
+    - [KEY_FILE_NAME].pub이 아닌 [KEY_FILE_NAME] 입니다!
+    - 35.216.91.122 는 서버 고정 아이피 주소입니다.
+    **********
+
+    ssh -i ~/.ssh/[KEY_FILE_NAME] [USERNAME]@35.216.91.122
+
+    를 입력하시고 접속하시면 됩니다!
+    '''
+    await ctx.send(strTemp)
+
 
 app.run(token)
